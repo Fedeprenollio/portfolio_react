1e4ed1bbfecb06d6e45567c318caa4e924aeb7cb
Federico-xaca <fede.preno@gmail.com>
2023-05-05 01:42:36 -0300

    add chat.gpt

--- src/pages/works/Works.jsx
+++ src/pages/works/Works.jsx
@@ -19,14 +19,23 @@ export const Works = () => {
       <h3>Componentes pequeños</h3>
       <h6>Mouse follower</h6>
      <iframe width={"100%"} src="https://react-practicas-mouse-follower.vercel.app/" frameborder="0"></iframe>
-     
+     <hr />
+
      <h6>Follower twitter buttons</h6>
      <iframe height={"250px"} width={"100%"} src="https://is-fallowint-twitter.vercel.app/" frameborder="0"></iframe>
+     <hr />
+
      <h6>Ta-te-ti</h6>
      <iframe height={"600px"} width={"100%"} src="https://ta-te-ti-blue.vercel.app/" frameborder="0"></iframe>
+     <hr />
 
      <h6>Traductor</h6>
      <iframe height={"600px"} width={"100%"} src="https://traductor-original.vercel.app/" frameborder="0"></iframe>
+     <hr />
+
+     <h6>Traductor con una IA</h6>
+     <p>Warning! La IA puede responder cosas inesperadas :D</p>
+     <iframe height={"600px"} width={"100%"} src="https://chat-gpt-ten-bay.vercel.app/" frameborder="0"></iframe>
     </div>
     </div>
   )
